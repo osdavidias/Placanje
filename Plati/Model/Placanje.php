071257d6cef93e19cb28ebb35d1c9dc8e818d8f3
osdavidias <osdavidias@gmail.com>
2016-07-27 19:40:16 +0200

    offline placanje

--- Plati/Model/Placanje.php
+++ Plati/Model/Placanje.php
@@ -1,6 +1,6 @@
 <?php
 namespace Explorer\Payment\Model;
-class Testpay extends \Magento\Payment\Model\Method\AbstractMethod
+class Placanje extends \Magento\Payment\Model\Method\AbstractMethod
 {
     protected $_code = 'placanje';
     protected $_isOffline = true;
